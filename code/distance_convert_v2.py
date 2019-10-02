cad60f52e4047e3fe8034eb3c8ec9caec80b9fb7
hannah <hannah.zz@qq.com>
2019-10-02 21:22:46 +0100

    lad class 1 code

---
+++ code/distance_convert_v2.py
@@ -0,0 +1,6 @@
+miles = input('please input miles: ')
+kilometers = float(miles) * 8 / 5
+
+print("Converting distance in miles to kilometers:")
+print("Distance in miles:     ", miles)
+print("Distance in kilometers:", kilometers)
