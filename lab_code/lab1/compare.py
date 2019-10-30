78e4ddfc10d7d4f9a17ee79fceb909529998526d
hannah <hannah.zz@qq.com>
2019-10-30 12:18:35 +0000

    lab1 solutions

--- lab_code/lab1/compare.py
+++ lab_code/lab1/compare.py
@@ -21,7 +21,7 @@ filenames = args
 
 if '-h' in opts:
     progname = sys.argv[0]
-    progname = progname.split('/')[-1] # strip out extended path
+    progname = progname.split('/')[-1]  # strip out extended path
     help = __doc__.replace('<PROGNAME>', progname, 1)
     print(help, file=sys.stderr)
     sys.exit()
@@ -40,9 +40,38 @@ print('INPUT-FILES:', ' '.join(filenames))
 stops = set()
 if '-s' in opts:
     with open(opts['-s'], 'r') as stop_fs:
-        for line in stop_fs :
+        for line in stop_fs:
             stops.add(line.strip())
 
 ##############################
 
+if '-b' in opts:
+    # sensitive of not
+    print(opts)
 
+
+# findall convert to lowercase
+# and then tran save to another files
+def txt_to_lower_case(filenames):
+    for name in filenames:
+        print(name)
+        with open(name, 'r') as fileinput:
+            for line in fileinput:
+                line = line.lower()
+                print(line)
+
+
+def count_words():
+    return 0
+
+
+def count_overlap():
+    return 0
+
+
+def count_overlap_sensitive():
+    return 0
+
+
+txt_to_lower_case(filenames)
+# compare.py -s stop_list.txt NEWS/news01.txt NEWS/news02.txt
--- test.py
+++
@@ -1,5 +0,0 @@
-import sys, re
-
-sent = 'I have baked a cake!'
-m = re.search(' (([a-z]+)(ed|ing)) ',sent)
-
